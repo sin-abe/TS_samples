9f4535448ab7e4b19e564db9e14eec8f11d558d8
220005 <220005ce@yse-c.net>
2023-11-29 11:40:32 +0900

    first commit

---
+++ TSBasic/first.js
@@ -0,0 +1,10 @@
+var message = "Hello, Why are you spinning onto my map?";
+console.log(message);
+var id = 1;
+var email = "testuser@test.com";
+var isActive = true;
+var none;
+console.log(id, typeof id);
+console.log(email, typeof email);
+console.log(isActive, typeof isActive);
+console.log(none, typeof none);
---
+++ TSBasic/first.ts
@@ -0,0 +1,12 @@
+var message:string = "Hello, Why are you spinning onto my map?";
+console.log(message);
+
+var id = 1;
+var email = "testuser@test.com";
+var isActive = true;
+var none;
+
+console.log(id, typeof id);
+console.log(email, typeof email);
+console.log(isActive, typeof isActive);
+console.log(none, typeof none);
