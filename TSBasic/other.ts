955bee4e2af8ecfbfa963d3b0db5ce660e478553
220005 <220005ce@yse-c.net>
2023-12-06 11:04:02 +0900

    add user,items,others

---
+++ TSBasic/other.ts
@@ -0,0 +1,11 @@
+
+var unionData: string|number;
+unionData = "Tokyo";
+unionData = 100;
+
+var data: any;
+data = 10;
+data = "Hi Stupid?";
+data = true;
+
+console.log(data, typeof data);
---
+++ TSBasic/user.ts
@@ -0,0 +1,22 @@
+// var user:{
+//     id: number;
+//     email: string;
+//     isActive: boolean;
+
+
+// } = {
+//     id: 1,
+//     email: "testuser@test.com",
+//     isActive: true
+// };
+
+var user = {
+    id: 1,
+    name: {first:"Taro",last: "Tokyo"},
+    email: "testuser@test.com",
+    isActive: true
+};
+
+console.log(user);
+
+
